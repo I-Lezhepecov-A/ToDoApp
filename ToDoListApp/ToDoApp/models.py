bdb9c687e18d0edb3cf6233eae5f97343134fe9e
Ilya Lezhepecov <770-175@mail.ru>
2020-07-23 14:12:43 +0300

    create models and make migrations

--- ToDoListApp/ToDoApp/models.py
+++ ToDoListApp/ToDoApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    pub_date=models.DateTimeField()
+    text= models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "ToDo"
