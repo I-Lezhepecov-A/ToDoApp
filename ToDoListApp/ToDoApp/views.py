a4cd551e905e5da8192a9aed72a03f95d13492cb
Ilya Lezhepecov <770-175@mail.ru>
2020-07-24 18:07:59 +0300

    add delete_todo func

--- ToDoListApp/ToDoApp/views.py
+++ ToDoListApp/ToDoApp/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from . import models
+from django.http import HttpResponseRedirect
 
 def home_page(request):
-    return render(request, template_name= 'ToDoApp/index.html')
+    todo_items = models.ToDo.objects.all().order_by("pub_date")[::-1]
+    return render(request, 'ToDoApp/index.html', {
+        "todo_items" : todo_items,
+    })
 
 @csrf_exempt
 def add_todo(request):
-    print (request)
-    return render(request, template_name= 'ToDoApp/index.html')
+    # print (request.POST)
+    added_date= timezone.now()
+    content = request.POST["content"]
+    create_obj=models.ToDo.objects.create( text = content, pub_date = added_date)
+    length_of_todo = models.ToDo.objects.all().count()
+    # print(length_of_todo)
+    # print(create_obj)
+    # print(create_obj.id) 
+    return HttpResponseRedirect("/")
+
+@csrf_exempt
+def delete_todo(request, todo_id):
+    models.ToDo.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect("/")
+
