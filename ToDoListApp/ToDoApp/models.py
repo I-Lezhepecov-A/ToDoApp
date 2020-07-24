1fec7a92e5bccc3a4c7d883172ba33ada18bd437
Ilya Lezhepecov <770-175@mail.ru>
2020-07-24 12:24:08 +0300

    add def __str__

--- ToDoListApp/ToDoApp/models.py
+++ ToDoListApp/ToDoApp/models.py
@@ -4,5 +4,8 @@ class ToDo(models.Model):
     pub_date=models.DateTimeField()
     text= models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.text
+
     class Meta:
         verbose_name_plural = "ToDo"
